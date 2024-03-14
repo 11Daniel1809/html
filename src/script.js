e6728ffca83afde5f972985c3b9ef4633feac89c
11Daniel1809 <ramirezjdaniel18@gmail.com>
2024-03-14 10:58:52 -0600

    condicionales, version1

---
+++ src/script.js
@@ -0,0 +1,55 @@
+var mensaje = "batir las fresas"
+var hay_fresas;
+var hay_peras;
+var hay_mangos;
+
+function jugo_de_fresas() {
+  window.alert(mensaje);
+  document.querySelector('#header').innerHTML = mensaje
+}
+
+
+function jugo_de_mango() {
+  window.alert(mensaje);
+  document.querySelector('#header').innerHTML = mensaje
+}
+
+
+
+function servir_vaso_de_agua() {
+  window.alert(mensaje);
+  document.querySelector('#header').innerHTML = mensaje
+}
+
+
+
+function jugo_de_peras() {
+  window.alert(mensaje);
+  document.querySelector('#header').innerHTML = mensaje
+
+}
+
+
+hay_fresas = false;
+hay_peras = false;
+hay_mangos = false;
+if (hay_fresas) {
+  mensaje = 'batir las fresas'
+  jugo_de_fresas();
+} else if (hay_peras) {
+  mensaje = 'batir las peras'
+  jugo_de_peras();
+}else if (hay_mangos) {
+  mensaje = 'batir los mangos'
+  jugo_de_mango();
+} else {
+  mensaje = 'sirvame el vaso de agua'
+  servir_vaso_de_agua();
+}
+
+
+
+
+
+
+
