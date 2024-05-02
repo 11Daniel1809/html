2740a1985614f2e3fdde1f5c52bfccb5f666ade8
11Daniel1809 <ramirezjdaniel18@gmail.com>
2024-05-02 10:58:31 -0600

    for2.js

---
+++ src/for2.js
@@ -0,0 +1,10 @@
+
+var número = []
+
+
+for (let index1 = 1; index1 <= 10; index1 ++) {
+
+    for (let index2 = 1; index2 <=10; index2 ++) {
+        console.log(index1+ " X "+index2 + " = "+número + index2)
+    }
+}
