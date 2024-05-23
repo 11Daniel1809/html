66bbc1bd755a908484f37539cd701ff207c1134a
11Daniel1809 <ramirezjdaniel18@gmail.com>
2024-05-23 16:03:46 -0600

    proyecto2.js

--- src/proyecto2.js
+++ src/proyecto2.js
@@ -13,6 +13,48 @@ function llenarArreglo() {
     console.log(arreglo)
 }
 
+function promedioArreglo(){
+let sumaTotal = 0;
+for(let i = 0; 1 < arreglo.length; i++){
+        sumaTotal = sumaTotal + arreglo[i];
+}  
+return sumaTotal / arreglo.length
+}
+
+function mayoresPromedio(){
+    let promedio = promedioArreglo();
+    let cantidadDeElementos = 0;
+
+    for(let i = 0; 1 < arreglo.length; i++){
+        if(arreglo[i] > promedio){
+            cantidadDeElementos++;
+        }
+    }
+    return cantidadDeElementos;
+}
+
+
+function sumaValor(aumentaArreglo){
+    arreglo.length = arreglo.length + aumentaArreglo
+}
+
+
+function sumaImpares(){
+    let totalImpares = 0
+
+    for(let i = 0; i < arreglo.length; i++){
+
+         if( (arreglo[i] % 2) != 0){
+            totalImpares = totalImpares + arreglo[i]
+         }
+
+    }
+    return totalImpares
+}
+
+
+
+
 var largo = parseInt(prompt("Digite el largo del arreglo"))
 construirArreglo(largo);
 llenarArreglo();
@@ -36,6 +78,35 @@ do {
              break;
         case 5://7
              console.log("La suma total de impares es: " + sumaImpares())
+             break;
+        case 6://8
+             console.log("El valor mayor del arreglo es: " + mayorArreglo())
+             break;
+        case 7://9
+             console.log("El valor absoluto mayor entre dos celdas consecutivas es: " + mayorDiferencia())
+             break;
+        case 8://10
+             let numeroConsulta = parseInt(prompt("¿Que numero deseas buscar en el arreglo?"))
+             console.log("El reesultado de la busqueda fue: " + encontrarElemento(numeroConsulta))
+             break;
+        case 9://10
+             let numeroConsultaPosicion = parseInt(prompt("¿Que numero deseas buscar en el arreglo para que retorne la posicion?"))
+             console.log("El resultado de la busqueda fue: " + posicionElemento(numeroConsultaPosicion))
+             break;
+        case 10://11
+             InvertirArreglo()
+             console.log("El arreglo fue invertido con exito")
+             break;
+        case 11://12
+             let posicion = parseInt(prompt("¿Que posicion desea sustituir?"))
+             let valor = parseInt(prompt("¿Que valor desea asignar?"))
+             insertarElemento(posicion, valor)
+             break;
+        case 12://13
+             mostrarHileraArreglo()
+             break;   
+        case 13://14
+             finalizarAplicacion()
              break;
 
     }
